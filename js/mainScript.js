c3dd041ca35ef2bd3afa63f5f2a7518ac242505f
dvbreva <jump.the.stars@abv.bg>
2019-07-15 01:18:12 +0300

    fix on lifelines so that they work better

--- js/mainScript.js
+++ js/mainScript.js
@@ -32,17 +32,15 @@ var generateQuestions = () => {
 
                 var count = 0;
                 var score = 0;
+                var userChoice; 
+                var answerIndex; 
+                // userChoice and answerIndex will be compared when the user selects an answer => "score" and "count" will be incremented if they match
 
-                var userChoice; // Stores users choice as the index of the selected answer in the array of all possible answers
-                var answerIndex; // Stores the index of the correct answer in the array of all possible answers
-                // Both variables compared when the user clicks next button. Score incremented if they match
-
-                // UPDATE QUESTION 
                 var allAnswers = [];
                 var correctAnswer = questionCollection[count].correct_answer;
-                $("#answerList").empty(); // Clear elements from previous question
+                $("#answerList").empty(); 
 
-                // Create an array from all of the answers
+                // Creates an array from all of the answers
                 questionCollection[count].incorrect_answers.forEach(function (answer) {
                     allAnswers.push(answer);
                 });
@@ -52,23 +50,29 @@ var generateQuestions = () => {
                 allAnswers.splice(Math.floor(Math.random() * 4), 0, correctAnswer);
                 allAnswers.join();
 
-                // Record the index of the correct answer in the array so it can be compared with the index of users choice
+                // Record the index of the correct answer so that it can be compared with the index of users choice
                 answerIndex = allAnswers.indexOf(correctAnswer);
 
-                // Append question to the DOM
                 $("#questionField").html(questionCollection[count].question);
 
-                for (var i = 0; i < allAnswers.length; i++) { // Loop through answers and append each to list
+                // Loop through answers and append each to the list
+                for (var i = 0; i < allAnswers.length; i++) { 
                     var li = document.createElement("li");
-                    var text = document.createTextNode(decodeHTML(allAnswers[i]));
+                    var text = document.createTextNode(decodeHtml(allAnswers[i]));
                     li.appendChild(text);
                     li.classList.add("answer");
                     li.id = i;
                     document.getElementById("answerList").appendChild(li);
                 }
 
-                // JOKERS
-                $('#jokerFriendBtn').click(function () { 
+                // LIFELINES
+                $("#lifelineFriendBtn").click(function () {
+                    var allAnswers = [];
+
+                    questionCollection[count].incorrect_answers.forEach(function (answer) {
+                        allAnswers.push(answer);
+                    });
+                  
                     var randomIndex = Math.floor(Math.random() * 4) + 1;
 
                     for (var i = 0; i < 4; i++) {
@@ -78,53 +82,71 @@ var generateQuestions = () => {
                             $("#suggestedAnswerByFriend").html("Suggested answer by your friend is: " + allAnswers[i]);
                         }
                     }
+
                     $(this).attr('disabled', true);
                 });
 
-                $("#jokerFiftyBtn").click(function () {
+                $("#lifelineFiftyBtn").click(function () {
+                    var allAnswers = [];
+
+                    var correctAnswer = questionCollection[count].correct_answer;
+
+                    questionCollection[count].incorrect_answers.forEach(function (answer) {
+                        allAnswers.push(answer);
+                    });
+
                     var indexToRemove = allAnswers.indexOf(correctAnswer);
-                    allAnswers.splice(indexToRemove,1);
-                    alert("The game says the right answer is not among " + allAnswers[1] +  " or " + allAnswers[2]);
-                    $("#suggestedAnswerByFifty").html("The game elimates the following answers: " + allAnswers[1] + " and " + allAnswers[2]);
+                    allAnswers.splice(indexToRemove, 1);
+
+                    alert("The game says the right answer is not among " + allAnswers[0] + " or " + allAnswers[1]);
+                    $("#suggestedAnswerByFifty").html("The game elimates the following answers: " + allAnswers[0] + " and " + allAnswers[1]);
                     $(this).attr('disabled', true);
                 });
 
-                $("#jokerPublicBtn").click(function () {
-                        alert("You have decided to ask the public.\n There are 70% votes for " + correctAnswer);
-                        $("#suggestedAnswerByPublic").html("Suggested answer by public is: " + correctAnswer);
-                        $(this).attr('disabled', true);
+                $("#lifelinePublicBtn").click(function () {
+                    var correctAnswer = questionCollection[count].correct_answer;
+
+                    questionCollection[count].incorrect_answers.forEach(function (answer) {
+                        allAnswers.push(answer);
+                    });
+                  
+                    alert("You have decided to ask the public.\n There are 70% votes for " + correctAnswer);
+                    $("#suggestedAnswerByPublic").html("Suggested answer by public is: " + correctAnswer);
+                    $(this).attr('disabled', true);
                 });
 
-                // SELECTING AN ANSWER 
+                // SELECTING AN ANSWER TO SUBMIT
                 document.querySelector("#answerList").addEventListener("click", function () {
                     var answers = document.getElementsByClassName("answer");
-                    if (event.target.id == "answerList") { // Prevent highlight from being applied to parent DIV
+                    if (event.target.id == "answerList") {
                         return;
                     }
-                    // Remove previous highlight
+
                     for (var i = 0; i < answers.length; i++) {
                         if (answers[i].classList.contains("selectedAnswer")) {
                             answers[i].classList.remove("selectedAnswer");
                         }
                     }
-                    // Highlight new selection
+
                     event.target.classList.add("selectedAnswer");
                     userChoice = event.target.id;
                 });
 
-
                 // SUMBITING ANSWER 
                 $("#nextBtn").click(function () {
                     var answers = document.getElementsByClassName("answer");
-                    if (userChoice == answerIndex) { // If users choice is correct...
-                        answers[userChoice].classList.add("correctAnswer"); // Highlight green
-                        score++;
-                        count++; // Increment question counter
+                    if (userChoice == answerIndex) { // if your choice is correct
+                        answers[userChoice].classList.add("correctAnswer"); 
 
-                        console.log("HI IM HERE");
-                        console.log(score);
+                        score++;
+                        count++; 
+
                         if (count < questionCollection.length) {
-                            $("#answerList").empty(); // Clear elements from previous question
+                            // should clear elements from previous question
+                            $("#answerList").empty(); 
+                            $("#suggestedAnswerByFriend").empty();
+                            $("#suggestedAnswerByPublic").empty();
+                            $("#suggestedAnswerByFifty").empty();
                             generateNewQuestion();
                             updateResult();
                         } else {
@@ -144,38 +166,39 @@ var generateQuestions = () => {
                         console.log("Game over.");
                     }
 
+                    // function for questions after the first one
                     function generateNewQuestion() {
 
                         var allAnswers = [];
                         var correctAnswer = questionCollection[count].correct_answer;
-                        $("#answerList").empty(); // Clear elements from previous question
+                        $("#answerList").empty(); 
+                        $("#suggestedAnswerByFriend").empty();
+                        $("#suggestedAnswerByPublic").empty();
+                        $("#suggestedAnswerByFifty").empty();
 
-                        // Create an array from all of the answers
                         questionCollection[count].incorrect_answers.forEach(function (answer) {
                             allAnswers.push(answer);
                         });
 
-                        // Push correct answer into array at random index
                         allAnswers.join();
                         allAnswers.splice(Math.floor(Math.random() * 4), 0, correctAnswer);
                         allAnswers.join();
 
-                        // Record the index of the correct answer in the array so it can be compared with the index of users choice
                         answerIndex = allAnswers.indexOf(correctAnswer);
 
-                        // Append question to the DOM
+                        // question
                         var countDisplay = count + 1;
                         $("#questionField").html("Question Number " + countDisplay + "<br>" + questionCollection[count].question);
 
-                        for (var i = 0; i < allAnswers.length; i++) { // Loop through answers and append each to list
+                        // answers
+                        for (var i = 0; i < allAnswers.length; i++) { 
                             var li = document.createElement("li");
-                            var text = document.createTextNode(decodeHTML(allAnswers[i]));
+                            var text = document.createTextNode(decodeHtml(allAnswers[i]));
                             li.appendChild(text);
                             li.classList.add("answer");
                             li.id = i;
                             document.getElementById("answerList").appendChild(li);
                         }
-
                     }
 
                     function updateResult() {
@@ -231,8 +254,6 @@ var generateQuestions = () => {
                         }
                     };
 
-
-
                 });
 
             });
@@ -249,28 +270,28 @@ var startGame = () => {
     generateQuestions();
 };
 
-function decodeHTML(html) {
+var decodeHtml = (html) => {
     var text = document.createElement("textarea");
     text.innerHTML = html;
     return text.value;
-}
+};
 
-// modal box logic
+// MODAL BOX
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("modalBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function () {
+btn.onclick = () => {
     modal.style.display = "block";
 }
 
-span.onclick = function () {
+span.onclick = () => {
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
